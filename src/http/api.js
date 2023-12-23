6802a8d982f9a36881d9ea2d7e3cc96911d3859e
LocNguyen <theghostz2612@outlook.com>
2023-12-23 18:13:22 +0700

    rewrite base url api

--- src/http/api.js
+++ src/http/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://laravel-api.com/api/v1/",
+    baseURL:"https://task-api.infinityfreeapp.com/api/v1/",
 
 })
 export default api
