a50547fd083dfe6b9e8f2954079f3c2f4ae52383
LocNguyen <theghostz2612@outlook.com>
2023-12-25 16:06:21 +0700

    connect api

--- src/http/api.js
+++ src/http/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"https://task-api.infinityfreeapp.com/api/v1/",
+    baseURL:"https://laravel-api.com/api/v1/",
 
 })
 export default api
